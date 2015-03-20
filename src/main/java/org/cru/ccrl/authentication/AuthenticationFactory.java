b172e3268ddd70ff6c79aeff81753211b978fd86
Lee Braddock <lee.braddock@cru.org>
2015-03-20 16:36:16 -0400

    Add AuthenticationFactory

---
+++ src/main/java/org/cru/ccrl/authentication/AuthenticationFactory.java
@@ -0,0 +1,17 @@
+package org.cru.ccrl.authentication;
+
+/**
+ * author@lee.braddock
+ */
+public class AuthenticationFactory
+{
+    public static Authentication getInstance(String host, String path)
+    {
+        AuthenticationImpl authentication = new AuthenticationImpl();
+
+        authentication.setHost(host);
+        authentication.setPath(path);
+
+        return authentication;
+    }
+}
